7a619b6fb03178809daf2fe3bda51ac2752dcff8
hongsonit10 <hongsonit10@gmail.com>
2025-03-02 19:24:19 +0700

    feat(extensions)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { CollaborativeApp } from "./CollaborativeApp";
-import { Room } from "./Room";
-
 export default function Home() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <h1 className="text-3xl font-bold text-green-400">Collaborative App</h1>
   );
 }
